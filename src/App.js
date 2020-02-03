ef97081da53eb7ad8860574b8894992d177436e6
Mateus Scverz <mate_fs@hotmail.com>
2020-02-03 10:25:39 -0300

    TUDO

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react';
+import Routes from './routes'
+import Header from './components/Header'
+import './styles.css';
+import Main from './pages/main'
+
+
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+)
+
+
+
+export default App;
